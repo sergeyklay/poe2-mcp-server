4153bd808e0649fcf6f66abd107408e0b8c2f43c
Serghei Iakovlev <git@serghei.pl>
2026-02-23 19:30:26 +0100

    chore: correct vitest config to produce junit coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['src/**/*.test.ts'],
+    outputFile: {
+      junit: 'coverage/junit.xml',
+    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov', 'json'],
