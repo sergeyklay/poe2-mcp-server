46c61a3e4b7b32fa9c661808eef7ca9e13538223
Serghei Iakovlev <git@serghei.pl>
2026-02-23 19:17:47 +0100

    chore: reformat vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.test.ts"],
+    include: ['src/**/*.test.ts'],
     coverage: {
-      provider: "v8",
-      reporter: ["text", "lcov", "json"],
-      reportsDirectory: "coverage",
-      include: ["src/**/*.ts"],
-      exclude: ["src/**/*.test.ts"],
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'json'],
+      reportsDirectory: 'coverage',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.test.ts'],
     },
   },
 });
